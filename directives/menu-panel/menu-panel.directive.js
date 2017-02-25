026ef289768e1c06219c02a5ae87debdccc69b2d
Nicholas Palenchar <nick@Nicks-pro.local>
2017-02-24 22:23:27 -0500

    basic annimation

---
+++ directives/menu-panel/menu-panel.directive.js
@@ -0,0 +1,11 @@
+nickpal.directive('npMenuPanel', function () {
+  return {
+    restrict: 'E',
+    templateUrl: 'directives/menu-panel/menu-panel.template.html',
+    scope: {
+      title: '=title',
+      titleBold: '=titleBold',
+      panelBackground: '=color'
+    },
+  }
+});
