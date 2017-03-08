bd8e614a301b0a1434ffeba7e5cfb5d2da338e28
Nicholas Palenchar <nick@Nicks-pro.local>
2017-03-07 21:14:30 -0500

    menu animation

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ let nickpal = angular.module("nickpal", ['ui.router'])
   .config(function($stateProvider){
     $stateProvider
       .state({
+        name: 'home',
+        url: '/'
+      })
+      .state({
         name: 'about',
         url: '/about',
         templateUrl: '/views/about.html'
@@ -13,6 +17,24 @@ let nickpal = angular.module("nickpal", ['ui.router'])
         templateUrl: '/views/npm/npm.html'
       })
   })
+.run(function ($rootScope) {
+
+  $rootScope.$on("$stateChangeSuccess", function (evt, toState) {
+    console.log("new transition\n", evt, toState);
+
+    var $menuPanel = $('.menu-panel');
+    if(toState.name === 'home')
+      $menuPanel.removeClass('collapsed');
+    else {
+      $menuPanel.addClass('collapsed force');
+      var $cachedMenu = $('.about-me', true);
+      setTimeout(function () {
+        $menuPanel.removeClass('force');
+      },1020)
+    }
+  })
+
+})
 
 .controller('main', function($scope){
   $scope.test = 'working';
