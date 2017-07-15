2e1a8090ce9744daa57bcb45f7ae97717048f5cd
Nicholas Palenchar <nick@Nicks-pro.local>
2017-07-15 14:23:49 -0400

    chpt

--- views/articles/articles.controller.js
+++ views/articles/articles.controller.js
@@ -1,3 +1,19 @@
-nickpal.controller('articlesCtrl',function($scope){
-  $scope.test2 = "WOLKING";
+nickpal.controller('articlesCtrl',function($scope, $http, Env){
+  $scope.articles = null;
+  $scope.baseUrl = 'http://' + Env.palenserver;
+
+  $http.get('http://'+Env.palenserver + '/articles')
+    .then(function(response){
+      $scope.articles = response.data;
+      try{
+        $scope.$digest();
+      }catch(e){
+        setTimeout($scope.$digest, 400);
+      }finally {
+        // parseMarkdown();
+      }
+    });
+
+  // setTimeout(function(){$scope.loading = false; $scope.$digest()}, 2000);
+
 });
